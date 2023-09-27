99fc143865e95fd408b796fec0a3baaa37780e1f
„SnowGRH” <gergelyovinczeffy@gmail.com>
2023-09-27 06:31:59 +0200

    a követelményeknek meg felel

--- src/gondolatolvaso/Gondolatolvaso.java
+++ src/gondolatolvaso/Gondolatolvaso.java
@@ -4,15 +4,29 @@ import java.util.Scanner;
 
 public class Gondolatolvaso {
 
-    static String[] pakli = new String[22];
-
     public static void main(String[] args) {
-        Kirak();
-        Kever();
-        EzVolt();
+        String[] pakli = Oszetesz();
+        for (int i = 0; i < 3; i++) {
+            Kirak(pakli);
+            int oszlop = Melyik();
+            pakli = Kever(pakli, oszlop);
+        }
+
+        EzVolt(pakli);
     }
 
-    public static void Kirak() {
+    private static void Kirak(String[] lapok) {
+        for (int i = 1; i < lapok.length; i++) {
+            System.out.printf("%-10s", lapok[i]);
+            if (i % 3 == 0) {
+                System.out.println("");
+            }
+        }
+
+    }
+
+    public static String[] Oszetesz() {
+        String[] pakli = new String[22];
         String[] szinek = {"P", "T", "Z", "M"};
         String[] ertek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
         int j = 1;
@@ -21,14 +35,7 @@ public class Gondolatolvaso {
                 pakli[j++] = ertek[i] + "." + szin;
             }
         }
-
-        for (int i = 1; i < pakli.length; i++) {
-            if (i % 3 == 1) {
-                System.out.println("");
-            }
-            System.out.printf("%-8s", pakli[i]);
-        }
-        System.out.println("\n");
+        return pakli;
     }
 
     public static int Melyik() {
@@ -43,62 +50,60 @@ public class Gondolatolvaso {
 
     }
 
-    public static void Kever() {
-        int bekert = Melyik();
-        int a;
-        int b;
-        int c;
-        String[] tomb = new String[22];
+    public static String[] Kever(String[] lapok, int bekert) {
+        String[] ujPakli = new String[lapok.length];
         switch (bekert) {
             case 1:
                 for (int i = 1; i <= 7; i++) {
-                    tomb[i] = pakli[20 - (i - 1) * 3];
-                    tomb[i + 7] = pakli[19 - (i - 1) * 3];
-                    tomb[i + 14] = pakli[21 - (i - 1) * 3];
+                    ujPakli[i] = lapok[20 - (i - 1) * 3];
+                    ujPakli[i + 7] = lapok[19 - (i - 1) * 3];
+                    ujPakli[i + 14] = lapok[21 - (i - 1) * 3];
 
                 }
-                for (int i = 1; i < tomb.length; i++) {
+                for (int i = 1; i < ujPakli.length; i++) {
                     if (i % 3 == 1) {
                         System.out.println("");
                     }
-                    System.out.printf("%-8s", tomb[i]);
+                    System.out.printf("%-8s", ujPakli[i]);
                 }
                 break;
             case 2:
                 for (int i = 1; i <= 7; i++) {
-                    tomb[i] = pakli[19 - (i - 1) * 3];
-                    tomb[i + 7] = pakli[20 - (i - 1) * 3];
-                    tomb[i + 14] = pakli[21 - (i - 1) * 3];
+                    ujPakli[i] = lapok[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = lapok[20 - (i - 1) * 3];
+                    ujPakli[i + 14] = lapok[21 - (i - 1) * 3];
 
                 }
-                for (int i = 1; i < tomb.length; i++) {
+                for (int i = 1; i < ujPakli.length; i++) {
                     if (i % 3 == 1) {
                         System.out.println("");
                     }
-                    System.out.printf("%-8s", tomb[i]);
+                    System.out.printf("%-8s", ujPakli[i]);
                 }
                 break;
 
             case 3:
 
                 for (int i = 1; i <= 7; i++) {
-                    tomb[i] = pakli[20 - (i - 1) * 3];
-                    tomb[i + 7] = pakli[21 - (i - 1) * 3];
-                    tomb[i + 14] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i] = lapok[20 - (i - 1) * 3];
+                    ujPakli[i + 7] = lapok[21 - (i - 1) * 3];
+                    ujPakli[i + 14] = lapok[19 - (i - 1) * 3];
                 }
 
-                for (int i = 1; i < tomb.length; i++) {
+                for (int i = 1; i < ujPakli.length; i++) {
                     if (i % 3 == 1) {
                         System.out.println("");
                     }
-                    System.out.printf("%-8s", tomb[i]);
+                    System.out.printf("%-8s", ujPakli[i]);
                 }
                 break;
         }
+        return ujPakli;
+
     }
 
-    public static void EzVolt() {
-
+    public static void EzVolt(String[] lap) {
+        System.out.printf("A kiválasztott kártya ez volt:", lap[11]);
     }
 
 }
